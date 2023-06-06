9bd155655a19bd4c49466091a9edc3b0d0f0c84a
Rudra Mahalik <rudra@qneuro.com>
2023-06-06 17:45:46 +0530

    initial commit

---
+++ ios/header.h
@@ -0,0 +1,16 @@
+//
+//  header.h
+//  Runner
+//
+//  Created by Rudra@Qneuro on 06/06/23.
+//
+
+#ifndef header_h
+#define header_h
+
+#include <optional>
+
+
+#endif /* header_h */
+
+
---
+++ ios/main.cpp
@@ -0,0 +1,11 @@
+//
+//  main.cpp
+//  Runner
+//
+//  Created by Rudra@Qneuro on 06/06/23.
+//
+
+#include <stdio.h>
+#include "header.h"
+
+std::optional<int> myOptional;
